87774f800fd1a9d505f9122344dc7ea06f20eb34
Martin Olveyra <molveyra@gmail.com>
2021-05-28 16:32:48 -0300

    updated to version 1.2.0

--- crawlera_session/__init__.py
+++ crawlera_session/__init__.py
@@ -12,7 +12,7 @@ from scrapy.downloadermiddlewares.redirect import RedirectMiddleware
 from scrapy.downloadermiddlewares.cookies import CookiesMiddleware
 
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 logger = logging.getLogger(__name__)
 
